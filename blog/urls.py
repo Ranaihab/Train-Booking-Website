14f7d66609869e7db8f3f492fdaff8478f4eea26
miretteamin <miretteamin@gmail.com>
2021-06-06 00:45:36 +0200

    addingRegistrationPages

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', views.home, name='home'),
+    path('Sign-Up/', views.signUp , name='signUp'),
+    path('Sign-In/', views.signIn , name='signIn')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.views.decorators.cache import never_cache
 
+@never_cache
 def home(request):
-    return render(request, 'sitePages/home.html', {'title': 'Home'})
+    return render(request, 'sitePages/home.html')
+
+@never_cache
+def signIn(request):
+    return render(request, 'registrationPages/signIn.html')
+
+@never_cache
+def signUp(request):
+    return render(request, 'registrationPages/SignUp.html')
