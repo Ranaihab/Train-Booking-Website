111280bb9d3316e04cd93288ac3969cb87dead7e
miretteamin <miretteamin@gmail.com>
2021-07-08 21:58:36 +0200

    addingSomeConstraints

--- TrainBookingWebsite/settings.py
+++ TrainBookingWebsite/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
+    'django_db_constraints',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.db.models.constraints import CheckConstraint
 from django.db.models.fields import IntegerField
 from django.db.models.fields.related import ForeignKey
+from django.db.models.query_utils import Q
+from django.db.models import F
 """from django.contrib.postgres.fields import ArrayField"""
 
 # Create your models here.
@@ -10,25 +13,41 @@ class User(models.Model):
     email = models.EmailField(max_length = 320)
     phone = models.CharField(max_length = 12, null = False, blank = False)
 
-class Stations(models.Model):
-    id = models.IntegerField(primary_key = True)
+class Station(models.Model):
+    id = models.IntegerField(default=-1, primary_key = True)
     stationName = models.CharField(max_length = 15)
 
 class Train(models.Model):
-    seatNum = models.IntegerField(null=False)
-    source = models.ForeignKey(Stations, on_delete = models.CASCADE, related_name = "TrainSource",null=False)
-    destination = models.ForeignKey(Stations, on_delete = models.CASCADE, related_name = "TrainDest",null=False)
+    seatNum = models.IntegerField(default=0)
+    source = models.ForeignKey(Station, on_delete = models.CASCADE, related_name = "TrainSource",null=False)
+    destination = models.ForeignKey(Station, on_delete = models.CASCADE, related_name = "TrainDest",null=False)
+
+    def getSource(self):
+        return self.source.id
+    def getDest(self):
+        return self.destination.id
+
 
 
 class Trip(models.Model):
-    source = models.ForeignKey(Stations, on_delete = models.CASCADE, related_name = "TripSource", null=False)
-    destination = models.ForeignKey(Stations, on_delete = models.CASCADE, related_name = "TripDest", null=False)
-    
     train = models.ForeignKey(Train, on_delete = models.CASCADE, related_name = "TripTrain")
+    source = models.ForeignKey(Station, on_delete = models.CASCADE, related_name = "TripSource", null=False)
+    destination = models.ForeignKey(Station, on_delete = models.CASCADE, related_name = "TripDest", null=False)
     day = models.DateField(null = False)
     startTime = models.TimeField(null = False)
     endTime = models.TimeField(null = False)
     price = models.FloatField(null = False, default=0)
+
+    def getSource(self):
+        return self.source
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                check=models.Q(source__gte = Train.getSource) and models.Q(source__lte = Train.getDest), name="sourceValid"),
+            models.CheckConstraint(
+                check=models.Q(destination__gte = Train.getSource) and models.Q(destination__lte = Train.getDest), name="destValid")
+        ]
 
 class Book(models.Model):
     trip = models.ForeignKey(Trip, on_delete = models.CASCADE, related_name = "BookTrips",null = False)
