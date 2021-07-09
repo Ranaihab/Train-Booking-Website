7260a38e206caab13fabfe25dbbaa644bffdeda5
Ranaihab <rana.ihab7@gmail.com>
2021-07-10 00:00:42 +0200

    Constraints added on trip

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Station, Train, Trip
 
-admin.site.register(Station)
-admin.site.register(Train)
-admin.site.register(Trip)
+class StationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'stationName')
+class TrainAdmin(admin.ModelAdmin):
+    list_display = ('id', 'Source', 'Destination', 'Number_of_seats')
+class TripAdmin(admin.ModelAdmin):
+    list_display = ('id', 'train', 'Source', 'Destination', 'day', 'start_Time', 'end_Time', 'price', 'Remaining_seats')
+admin.site.register(Station, StationAdmin)
+admin.site.register(Train, TrainAdmin)
+admin.site.register(Trip, TripAdmin)
