91fd4ece182d1b267641ae18b20c31c1aeaa1c2b
habiba157 <77742360+habiba157@users.noreply.github.com>
2021-07-10 18:21:23 +0200

    ch

---
+++ static/js/check.js
@@ -0,0 +1,34 @@
+function check(form) {
+
+
+    if (form.textfield2.value.length > 30) {
+        alert("Error : Too Long! ");
+        form.textfield2.focus();
+        return false;
+
+    }
+    if (form.textfield.value.length > 30) {
+        alert("Error : Too Long! ");
+        form.textfield2.focus();
+        return false;
+
+    }
+    if (form.textfield3.value.length > 30) {
+        alert("Error : Too Long! ");
+        form.textfield2.focus();
+        return false;
+
+    }
+    if (form.textfield4.value.length > 30) {
+        alert("Error : Too Long! ");
+        form.textfield2.focus();
+        return false;
+
+    }
+    if (form.textfield5.value.length > 30) {
+        alert("Error : Too Long! ");
+        form.textfield2.focus();
+        return false;
+
+    }
+}
---
+++ static/js/options.js
@@ -0,0 +1,14 @@
+document.getElementById("op1").style.display = 'none';
+document.getElementById("op2").style.display = 'none';
+document.getElementById("op3").style.display = 'none';
+document.getElementById("op4").style.display = 'none';
+document.getElementById("op5").style.display = 'none';
+
+function date() {
+    document.getElementById("op1").style.display = 'block';
+}
+
+function time() {
+    document.getElementById("op2").style.display = 'block';
+
+}
