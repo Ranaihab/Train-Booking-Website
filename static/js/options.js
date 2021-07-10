90447fd7afde45d076dd500d584d35e762e7a5ec
habiba157 <77742360+habiba157@users.noreply.github.com>
2021-07-10 20:54:34 +0200

    try

--- static/js/options.js
+++ static/js/options.js
@@ -1,14 +1,62 @@
-document.getElementById("op1").style.display = 'none';
-document.getElementById("op2").style.display = 'none';
-document.getElementById("op3").style.display = 'none';
-document.getElementById("op4").style.display = 'none';
-document.getElementById("op5").style.display = 'none';
+function edit(obj, id1, id2, id3, id4, id5) {
+    text = obj.options[obj.selectedIndex].value;
 
-function date() {
-    document.getElementById("op1").style.display = 'block';
-}
+    document.getElementById("op1").style.display = 'none';
+    document.getElementById("op2").style.display = 'none';
+    document.getElementById("op3").style.display = 'none';
+    document.getElementById("op4").style.display = 'none';
+    document.getElementById("op5").style.display = 'none';
 
-function time() {
-    document.getElementById("op2").style.display = 'block';
+    if (text.match(op1)) {
+        document.getElementById("op1").style.display = 'block';
 
+    }
+    if (text.match(op2)) {
+        document.getElementById("op2").style.display = 'block';
+
+    }
+
+    if (text.match(op3)) {
+        document.getElementById("op3").style.display = 'block';
+
+    }
+
+    if (text.match(op4)) {
+        document.getElementById("op4").style.display = 'block';
+
+    }
+
+    if (text.match(op5)) {
+        document.getElementById("op5").style.display = 'block';
+
+    }
+
+    /** 
+    function date() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange =
+
+            function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    document.getElementById("da").innerHTML = this.responseText;
+                }
+
+            };
+        xhttp.open("GET", "date.html", true);
+        xhttp.send();
+    }
+
+    function time() {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange =
+
+            function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    document.getElementById("ti").innerHTML = this.responseText;
+                }
+
+            };
+        xhttp.open("GET", "time.html", true);
+        xhttp.send();
+    }**/
 }
